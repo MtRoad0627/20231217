7b5d7333a8448a195509315ce2edaa6fbad8d195
Ryo Y <ryoy@RyonoMacBook-Air-2.local>
2023-12-17 15:22:48 +0900

    Setcolor

--- Src/main.js
+++ Src/main.js
@@ -1,4 +1,10 @@
 async function main() {
     DrawText("sjbhfkjshsurhfiua", 100, 100)
     DrawText(12847, 300, 500)
+
+    SetColor("red")
+    DrawText("あああ", 100, 300)
+
+    SetColor("#33FFCA")
+    DrawText("kjfshk", 300, 300)
 }
