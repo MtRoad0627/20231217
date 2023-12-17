7143e3eeed77fbc82ef97036ea7fde9571de2e98
Ryo Y <ryoy@RyonoMacBook-Air-2.local>
2023-12-17 15:25:49 +0900

    calculate

--- Src/main.js
+++ Src/main.js
@@ -1,10 +1,15 @@
 async function main() {
-    DrawText("sjbhfkjshsurhfiua", 100, 100)
-    DrawText(12847, 300, 500)
+    // DrawText("sjbhfkjshsurhfiua", 100, 100)
+    // DrawText(12847, 300, 500)
 
-    SetColor("red")
-    DrawText("あああ", 100, 300)
+    // SetColor("red")
+    // DrawText("あああ", 100, 300)
 
-    SetColor("#33FFCA")
-    DrawText("kjfshk", 300, 300)
+    // SetColor("#33FFCA")
+    // DrawText("kjfshk", 300, 300)
+
+    //計算
+    DrawText(1+1, 300, 300)
+    DrawText(1-1, 400, 400)
+    DrawText(3%2, 200, 200)
 }
